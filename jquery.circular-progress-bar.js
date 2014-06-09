2502a8d144cc4e07bac3278c9709350478b09d3c
jurre <jurrestender@hotmail.com>
2014-06-09 13:35:52 +0200

    pass an options hash/object to init

--- jquery.circular-progress-bar.js
+++ jquery.circular-progress-bar.js
@@ -1,76 +1,76 @@
 var circularProgressBar = (function($) {
   function polarToCartesian(centerX, centerY, radius, percent) {
     var percent = percent - 25, // make the starting point top center
-      angleInRadians = (percent * .01 * 360) * Math.PI / 180.0,
-      x = centerX + radius * Math.cos(angleInRadians),
-      y = centerY + radius * Math.sin(angleInRadians);
-    
+    angleInRadians = (percent * .01 * 360) * Math.PI / 180.0,
+    x = centerX + radius * Math.cos(angleInRadians),
+    y = centerY + radius * Math.sin(angleInRadians);
+
     return {
       x: x,
       y: y
     }
   }
-  
+
   function CircularProgressBar($container, radius, width, activeColor, inactiveColor, innerColor, initialPercentage) {
     this.radius = radius;
     this.$el = $('<svg xmlns="http://www.w3.org/2000/svg" version="1.1">' +
-      '<circle cx="' + radius + '" cy="' + radius + '" r="' + radius +'"' +
-      'fill="' + inactiveColor + '"></circle>' +
-      '<path fill="' + activeColor + '"></path>' + 
-      '<circle cx="' + radius + '" cy="' + radius + '" r="' +
-      (radius - width) + '" fill="' + innerColor + '"></circle></svg');
-    
-    this.$progressCircle = $('path', this.$el);
-    
-    this.setValue(initialPercentage);
-    
-    if ($container) {
-      $container.append(this.$el);
-    }
+                 '<circle cx="' + radius + '" cy="' + radius + '" r="' + radius +'"' +
+                   'fill="' + inactiveColor + '"></circle>' +
+                   '<path fill="' + activeColor + '"></path>' + 
+                   '<circle cx="' + radius + '" cy="' + radius + '" r="' +
+                   (radius - width) + '" fill="' + innerColor + '"></circle></svg');
+
+                 this.$progressCircle = $('path', this.$el);
+
+                 this.setValue(initialPercentage);
+
+                 if ($container) {
+                   $container.append(this.$el);
+                 }
   }
-  
+
   CircularProgressBar.prototype.renderPathData = function(percent) {
     var rendered,
-      radius = this.radius,
-      template = "M" + radius + "," + radius + " L"  + radius + ",0 " + "A" + radius + "," + radius + " 0 {largeArcFlag},1 {x},{y}",
-      coords = polarToCartesian(radius, radius, radius, percent);
-      
+    radius = this.radius,
+    template = "M" + radius + "," + radius + " L"  + radius + ",0 " + "A" + radius + "," + radius + " 0 {largeArcFlag},1 {x},{y}",
+    coords = polarToCartesian(radius, radius, radius, percent);
+
     rendered = template.replace('{largeArcFlag}', percent > 50 ? '1' : '0');
     rendered = rendered.replace('{x}', coords.x);
     rendered = rendered.replace('{y}', coords.y);
-    
+
     return rendered;
   }
-  
+
   CircularProgressBar.prototype.setValue = function(percent) {
     percent = percent >= 100 ? 99.99999 : percent;
     percent = percent < 0 ? 0 : percent;
-    
+
     this.$progressCircle.attr('d', this.renderPathData(percent));
   }
-  
-  function init(radius, width, $container, activeColor, inactiveColor, innerColor, initalPercentage) {
-    radius = parseInt(radius);
-    width = parseInt(width);
-    initalPercentage = parseInt(initalPercentage);
-    
+
+  function init(args) {
+    var radius = parseInt(args.radius, 10) || 150;
+    var $container = args.container || $();
+    var width = parseInt(args.width, 10) || 20;
+    var initialPercentage = parseInt(args.initialPercentage, 10);
+    var activeColor = args.activeColor || 'orange';
+    var inactiveColor = args.inactiveColor || "#d1d1d1";
+    var innerColor = args.innerColor || '#ffffff';
+
     if (!($.isNumeric(radius) && radius > 0)) {
       throw new Exception('circularProgress : init() : Radius must be a positive number.');
     } else if (!($.isNumeric(width) && width > 0)) {
       throw new Exception('circularProgress : init() : width must be a positive number.');
-    }  
-    
-    $container = $container || $();
-    activeColor = activeColor || "orange";
-    inactiveColor = inactiveColor || "#d1d1d1";
-    innerColor = innerColor || "white";
-    initalPercentage = $.isNumeric(initalPercentage) ? initalPercentage : 0;
-    
+    }
+
+    initialPercentage = $.isNumeric(initialPercentage) ? initialPercentage : 0;
+
     return new CircularProgressBar(
-            $container, radius, width, activeColor, inactiveColor, innerColor, initalPercentage
-        );
+      $container, radius, width, activeColor, inactiveColor, innerColor, initialPercentage
+    );
   }
-  
+
   return {
     init: init
   }
